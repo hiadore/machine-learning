ab958c26eb1e5bb5731e2b6670442648c52793a9
hiadore <abdullah.husein9192@gmail.com>
2018-07-21 22:29:01 +0700

    Submit ex1 opt feature normalization

--- Machine Learning - Andrew NG/machine-learning-ex1/ex1/featureNormalize.m
+++ Machine Learning - Andrew NG/machine-learning-ex1/ex1/featureNormalize.m
@@ -6,10 +6,13 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   working with learning algorithms.
 
 % You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
 
+% X_norm = X;
+% mu = zeros(1, size(X, 2));
+% sigma = zeros(1, size(X, 2));
+
+mu = mean(X);
+sigma = std(X);
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
@@ -26,13 +29,7 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
-
-
-
-
+X_norm = (X - mu) ./ sigma;
 
 % ============================================================
 
